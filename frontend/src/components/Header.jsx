c7304bee82caa74bcf5f4286880f94b5378f5004
ChiranYt <chiranjeevi@adsyzygy.com>
2025-02-10 18:29:49 +0530

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -35,7 +35,9 @@ const Header = () => {
             <img
               src="https://adsyzygy.com/img/webp/logo.jpg"
               alt="Logo"
-              className="logo-img"
+             width="150"
+      height="50"
+      className="logo-img"
             />
           </a>
           <div id="navbarNav">
